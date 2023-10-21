80706c1265f6d38b499d171a29376545d8624830
Pradyumna Shome <pradyumna.shome@gmail.com>
2023-10-20 20:41:25 -0400

    Update people.js

--- src/components/people.js
+++ src/components/people.js
@@ -11,20 +11,20 @@ const allPeople = {
     "Post-Doctoral Researchers": [],
     "PhD Students": [
         {dblpName: "Stephan van Schaik", email: "stephvs@umich.edu", website: "https://synkhronix.com", role: "PhD Student", photo: defaultProfilePhoto},
-        {dblpName: "Andrew Kwong", email: "ankwong@umich.edu", website: "https://andrewkwong.org/", role: "PhD Student", photo: defaultProfilePhoto},
         {dblpName: "Youssef Toubah", email: "ytobah@umich.edu", website: "/", role: "PhD Student", photo: defaultProfilePhoto},
         {dblpName: "Ingab Kang", email: "igkang@umich.edu", website: "https://scholar.google.com/citations?user=ik1D_PUAAAAJ&hl=en", role: "PhD Student", photo: defaultProfilePhoto},
         {dblpName: "Jason Kim", email: "nosajmik@gatech.edu", website: "https://jasonkim.page", role: "PhD Student", photo: defaultProfilePhoto},
-        {dblpName: "Jie Jeff Xu", email: "jxu680@gatech.edu", website: "/", role: "PhD Student", photo: defaultProfilePhoto},
         {dblpName: "Nureddin Kamadan", email: "kamadan@gatech.edu", website: "/", role: "PhD Student", photo: defaultProfilePhoto},
-        {dblpName: "Pradyumna Shome", email: "pradyumna.shome@gatech.edu", website: "https://pradyumnashome.com", role: "PhD Student", photo: defaultProfilePhoto}
+        {dblpName: "Pradyumna Shome", email: "pradyumna.shome@gatech.edu", website: "https://pradyumnashome.com", role: "PhD Student", photo: defaultProfilePhoto},
+        {dblpName: "Hritvik Taneja", email: "htaneja3@gatech.edu", website: "/", role: "PhD Student", photo: defaultProfilePhoto},
+        {dblpName: "Walter Wang", email: "walwan@gatech.edu", website: "/", role: "PhD Student", photo: defaultProfilePhoto},
+        {dblpName: "Jalen Chuang", email: "jchuang9@gatech.edu", website: "/", role: "PhD Student", photo: defaultProfilePhoto},
+        {dblpName: "Zeezoo Ryu", email: "zeezooryu@gatech.edu", website: "/", role: "PhD Student", photo: defaultProfilePhoto}]
     ],
     "MS Students": [
-    {dblpName: "Hritvik Taneja", email: "htaneja3@gatech.edu", website: "/", role: "MS Student", photo: defaultProfilePhoto},
     {dblpName: "Byeongyong Go", email: "bygo@gatech.edu", website: "/", role: "MS Student", photo: defaultProfilePhoto},
-    {dblpName: "Jakub Jackowiak", email: "jjackowiak3@gatech.edu", website: "/", role: "MS Student", photo: defaultProfilePhoto}],
-    "Undergraduates": [
-    {dblpName: "Walter Wang", email: "walwang@umich.edu", website: "/", role: "Undergraduate", photo: defaultProfilePhoto}]
+    {dblpName: "Sam Lefforge", email: "plefforge3@gatech.edu", website: "/", role: "MS Student", photo: defaultProfilePhoto}],
+    "Undergraduates": []
 };
 
 const PeopleList = (props) => {
